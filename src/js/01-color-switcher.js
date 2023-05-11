ac85bbc7ec556540e76d87f3722b2c05f9456e7a
Brod9ga <gbonik@i.ua>
2023-05-12 00:02:45 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+ 
+    const startBtn= document.querySelector('[data-start]')
+    const stopBtn= document.querySelector('[data-stop]')
+
+// console.log(refs.startBtn)
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  
+ startBtn.addEventListener('click', ()=>{
+     interval = setInterval(()=>
+        {document.body.style.backgroundColor = getRandomHexColor()}, 1000
+    )
+    
+    startBtn.disabled = true
+    stopBtn.disabled = false
+ })
+
+ stopBtn.addEventListener('click', ()=>{
+    clearInterval(interval)
+    startBtn.disabled = false
+    stopBtn.disabled = true 
+ })
