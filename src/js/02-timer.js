81a274123ac17a731986d6a1f7d5f2949b994034
Brod9ga <gbonik@i.ua>
2023-05-12 02:17:12 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,35 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const inputEl = document.querySelector('#datetime-picker')
+const startBtn = document.querySelector('[data-start]')
+const day = document.querySelector('[data-days]')
+const hour = document.querySelector('[data-hours]')
+const min = document.querySelector('[data-minutes]')
+const sec = document.querySelector('[data-seconds]')
 // console.log(inputEl)
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
+startBtn.addEventListener('click', ()=>{runTimer()})
 
-  };
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate < new Date()) {
+      Notify.failure('Please choose a date in the future');
+      // window.alert('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
   
-  flatpickr(inputEl, options)
 
   function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -34,4 +49,35 @@ const options = {
   
     return { days, hours, minutes, seconds };
   }
-  
+
+function padStart(value) {
+  return value.toString().padStart(2, '0');
+}
+
+  function runTimer() {
+    intervalIdTimer = setInterval(()=>{
+    const newDate = new Date(inputEl.value)
+    const date = new Date()
+        const difDate = newDate.getTime() - date.getTime()
+        
+    if (difDate<=0){
+        startBtn.disabled = false 
+        clearInterval(intervalIdTimer)
+        day.textContent = '00'
+        min.textContent = '00'
+        sec.textContent = '00'
+        hour.textContent = '00'
+        return
+        }
+        startBtn.disabled = true
+
+        const { days, hours, minutes, seconds } = convertMs(difDate)
+        
+    day.textContent = padStart(days);
+    hour.textContent = padStart(hours);
+    min.textContent = padStart(minutes)
+    sec.textContent = padStart(seconds)
+    },1000)
+    }
+    
+  flatpickr(inputEl, options)
