1c9cd15481abd0525a90f4600c32a68e5bb0943e
Brod9ga <gbonik@i.ua>
2023-05-12 23:04:29 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,14 +4,14 @@
     const stopBtn= document.querySelector('[data-stop]')
 
 
-
+let intervalId
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
   
  startBtn.addEventListener('click', ()=>{
-    let intervalId = setInterval(() => {document.body.style.backgroundColor = getRandomHexColor()}, 1000)
+    intervalId = setInterval(() => {document.body.style.backgroundColor = getRandomHexColor()}, 1000)
     
     startBtn.disabled = true
     stopBtn.disabled = false
